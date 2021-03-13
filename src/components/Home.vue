1d048eb30a6e582571a8375c0ae5dd0638e568aa
Shubham-Lodhi <shubhamlodhi714@gmail.com>
2021-03-13 13:13:07 +0530

    Events and Methods

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,9 @@
     <h1>Hello From Home Component</h1>
     <h2>{{data}}</h2>
     <h3>{{msg}}</h3>
+    <button v-on:click="hello">Call Hello Function</button>
+    <button v-on:click="hello2('Hello from hello2 function')">Call Hello2 Function</button>
+    <button v-on:mouseover="hello2('Hello from hello2 function')">Call Hello2 Function</button>
   </div>
 </template>
 
@@ -12,6 +15,15 @@ export default {
   props: {
     data: String,
     msg: String
+  },
+  methods: {
+    hello() {
+      console.log("Hello Function Called Console");
+    },
+
+    hello2(data) {
+      alert(data);
+    }
   }
 };
 </script>
